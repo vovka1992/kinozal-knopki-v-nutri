a2d1fadd4da4b32d79e2b932059bf7f9b2a63099
Vladimir <62159509+vovka1992@users.noreply.github.com>
2020-12-15 00:02:20 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+alert('test');
 GM_addStyle(`
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css";
 @import "https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css";
